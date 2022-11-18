18af42abc790e0ee53068a2e25fd36abb7a31800
AmandaFrisk <amanda.frisk33@gmail.com>
2022-11-18 13:53:53 -0700

    Atlas connection

--- config/db.connection.js
+++ config/db.connection.js
@@ -2,6 +2,7 @@ const mongoose = require('mongoose');
 
 const connectionStr = process.env.MONGODB_URI || 'mongodb://localhost:27017/profilesDB';
 
+
 // set up connection with the DB
 mongoose.connect( connectionStr);
 
