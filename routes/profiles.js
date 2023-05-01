ae316b00f8f4fc14af54a1ede66ecf16df978fc6
laurenhatchett <laurenhatchett@gmail.com>
2023-04-30 22:03:44 -0400

    add middleware/auth.js and update profile routes

--- routes/profiles.js
+++ routes/profiles.js
@@ -1,12 +1,22 @@
-const express = require('express');
-const router = express.Router();
+import { Router } from 'express'
+import * as profilesCtrl from '../controllers/profiles.js'
+import { decodeUserFromToken, checkAuth } from '../middleware/auth.js'
 
-const ctrls = require('../controllers')
 
-router.get('/', ctrls.profiles.index);
-router.post('/', ctrls.profiles.create);
-router.delete('/:id', ctrls.profiles.destroy);
-router.put('/:id', ctrls.profiles.update );
-router.get('/:id', ctrls.profiles.show );
-module.exports = router
- 
+const router = Router()
+
+/*-------- Public Routes -----------*/
+
+
+/*-------- Protected Routes -----------*/
+
+// const ctrls = require('../controllers')
+
+// router.get('/', ctrls.profiles.index);
+// router.post('/', ctrls.profiles.create);
+// router.delete('/:id', ctrls.profiles.destroy);
+// router.put('/:id', ctrls.profiles.update );
+// router.get('/:id', ctrls.profiles.show );
+
+
+export { router }
