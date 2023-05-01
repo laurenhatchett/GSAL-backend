df28499bdad5d88dc2d76a151a9b85616cdf19f5
laurenhatchett <laurenhatchett@gmail.com>
2023-04-30 21:16:01 -0400

    reconfigure config/database.js and server.js

--- server.js
+++ server.js
@@ -1,34 +1,26 @@
-/* == External Modules == */
-const express = require('express')
-const cors = require('cors')
-// whitelist & corsOptions
-const whitelist = ['http://localhost:3000',`${process.env.FRONTEND_URL}`];
-// const corsOptions = {
-//   origin: function (origin, callback) {
-//     if (whitelist.indexOf(origin) !== -1 || (!origin)) {
-//       callback(null, true)
-//     } else {
-//       callback(new Error('Not allowed by CORS'))
-//     }
-//   }
-// }
+
+import 'dotenv.config.js'
+import express from 'express'
+import cors from 'cors'
+
+import './confirg/database.js'
+
+const app = express()
+
+
+/*----------------*/
+
+
 
 require('dotenv').config()
 /* == Internal Modules == */
 const routes = require('./routes');
 
-/* == Express Instance == */
-const app = express()
 
-/* == Port == */
-const PORT = process.env.PORT || 3003;
 
-/* == DB connection == */
-require('./config/db.connection');
 
-/* == Middleware == */
-// app.use(cors(corsOptions)) 
-app.use(cors("*")) 
+
+app.use(cors()) 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
