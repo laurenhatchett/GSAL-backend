2882214c27f696fe3a75981c058d81d2baef9cef
laurenhatchett <laurenhatchett@gmail.com>
2023-04-30 21:47:19 -0400

    install npm modules and update server.js

--- server.js
+++ server.js
@@ -2,39 +2,29 @@
 import 'dotenv.config.js'
 import express from 'express'
 import cors from 'cors'
-
+import logger from 'morgan'
+import formData from 'express-form-data'
 
 import { router as profilesRouter } from './routes/profiles.js'
-
 
 import './confirg/database.js'
 
 const app = express()
 
-
-/*----------------*/
-
-
-
-require('dotenv').config()
-/* == Internal Modules == */
-const routes = require('./routes');
-
-
-
-
-
 app.use(cors()) 
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
+app.use(logger('dev'))
+app.use(express.json())
+app.use(formData.parse())
 
-/* == Routes == */
-app.get('/', function (req, res) {
-	res.send('hello');
-});
-app.use("/profiles",routes.profiles)
+app.use('/api/profiles', profilesRouter)
 
-/* == Listen == */
-app.listen(PORT, () => {
-  console.log('🎉🎊', 'Party on PORT', PORT, '🎉🎊',)
+
+app.use(function (req, res, next) {
+  res.status(404).json({ err: 'Not found' })
 })
+
+app.use(function (err, req, res, next) {
+  res.status(err.status || 500).json({ err:err.message })
+})
+
+export { app }
